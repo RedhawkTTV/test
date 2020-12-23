f86e489e5889c589b5a3c8a1491feb22617bb063
RedhawkTTV <74086572+RedhawkTTV@users.noreply.github.com>
2020-12-22 19:24:03 -0500

    test

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+string noun;
+string adj;
+string verb;
+string done;
+int main(){
+
+  cout<<"adjective";
+  cin>> adj;
+  cout<<"verb in the past tense";
+  cin>> verb;
+  cout<<"noun";
+  cin>>noun;
+  cout<<"The "<<adj<<" cat "<<verb<<" to the "<<noun<<".\n";
+  cout<<"type done when done!";
+  cin>> done;
+  cout<< done;
+
+ return 0;
+}
+
